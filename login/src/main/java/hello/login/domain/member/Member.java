d1c07f0405b7ddb7ef635ddaad74a6582cc3c2f9
neolbo <wjddlstjs09@naver.com>
2024-02-28 12:05:33 +0900

    회원가입

---
+++ login/src/main/java/hello/login/domain/member/Member.java
@@ -0,0 +1,16 @@
+package hello.login.domain.member;
+
+import jakarta.validation.constraints.NotEmpty;
+import lombok.Data;
+
+@Data
+public class Member {
+    private Long id;
+
+    @NotEmpty
+    private String loginId;
+    @NotEmpty
+    private String password;
+    @NotEmpty
+    private String name;
+}
