15f09f3cb978b788a0a4313e8b4f63b93bad0adb
neolbo <wjddlstjs09@naver.com>
2024-02-28 17:40:04 +0900

    Login_My Session Manager

--- login/src/main/java/hello/login/web/HomeController.java
+++ login/src/main/java/hello/login/web/HomeController.java
@@ -2,6 +2,8 @@ package hello.login.web;
 
 import hello.login.domain.member.Member;
 import hello.login.domain.member.MemoryMemberRepository;
+import hello.login.web.session.SessionManager;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
@@ -15,13 +17,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class HomeController {
 
     private final MemoryMemberRepository memberRepository;
+    private final SessionManager sessionManager;
 
     //    @GetMapping("/")
     public String home() {
         return "home";
     }
 
-    @GetMapping("/")
+    //    @GetMapping("/")
     public String homeLogin(@CookieValue(name = "memberId", required = false) Long memberId, Model model) {
         if (memberId == null) {
             // 로그인 하지 않은 화면
@@ -37,6 +40,21 @@ public class HomeController {
 
         // 로그인 성공
         model.addAttribute("member", loginMember);
+        return "loginHome";
+    }
+
+    @GetMapping("/")
+    public String homeLoginV2(HttpServletRequest request, Model model) {
+
+        // 세션 관리자에 의해 저장된 회원 정보 조회
+        Member member = (Member)sessionManager.getSession(request);
+        if (member == null) {
+            // 로그인 하지 않은 화면
+            return "home";
+        }
+
+        // 로그인 성공
+        model.addAttribute("member", member);
         return "loginHome";
     }
 }
