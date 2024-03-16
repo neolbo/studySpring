348d9586be7d6ed6ebd0bd10626c0d21a7fd805f
neolbo <wjddlstjs09@naver.com>
2024-03-16 10:42:41 +0900

    ResponseStatusExceptionResolver

--- exception/src/main/java/hello/exception/api/ApiExceptionController.java
+++ exception/src/main/java/hello/exception/api/ApiExceptionController.java
@@ -1,19 +1,23 @@
 package hello.exception.api;
 
+import hello.exception.exception.BadRequestException;
 import hello.exception.exception.UserException;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 @Slf4j
 @RestController
+@RequestMapping("/api")
 public class ApiExceptionController {
 
-    @RequestMapping("/api/members/{id}")
+    @RequestMapping("/members/{id}")
     public MemberDto getMember(@PathVariable String id) {
         if (id.equals("ex")) {
             throw new RuntimeException("잘못된 사용자");
@@ -28,6 +32,16 @@ public class ApiExceptionController {
         }
 
         return new MemberDto(id, "hello " + id);
+    }
+
+    @GetMapping("response-status-ex1")
+    public String responseStatusEx1() {
+        throw new BadRequestException();
+    }
+
+    @GetMapping("response-status-ex2")
+    public String responseStatusEx2() {
+        throw new ResponseStatusException(HttpStatus.NOT_FOUND, "error.bad", new IllegalArgumentException());
     }
 
     @Data
